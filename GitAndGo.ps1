4e6744212f3f0a5ac1f783cb3cb9e4b22daa97eb
KalleyatWork <stephen.vaughn2@va.gov>
2023-01-23 08:33:39 -0700

    GitAndGo Label
    Signed-off-by: KalleyatWork <stephen.vaughn2@va.gov>

---
+++ GitAndGo.ps1
@@ -0,0 +1,5 @@
+﻿_github-pages-challenge-SteveofCaley
+1. Create a TXT record in your DNS configuration for the following hostname: 
+2. Use this code for the value of the TXT record: 
+3. Wait until your DNS configuration changes. This could take up to 24 hours to propagate.
+  
